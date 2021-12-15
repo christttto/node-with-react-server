129fe835c8ade114451235b850c1a76c19b18443
Chris <chris_kim@rapid7.com>
2021-12-14 16:20:20 -0800

    wip passport

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 //we might have several different of these things
 //though it is usually just one
 
-app.get('/', (req, res) =>{
-    res.send({ bye: 'buddy'});
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
