66609cdbae7e400c47ca1822cf0fc8b14f9c99ed
Chris <chris_kim@rapid7.com>
2022-01-24 23:12:45 -0800

    21 wip

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+/*
+app.get('/', (req, res) =>{
+    res.send({bye: 'buddy'});
+});
+*/
 const app = express();
 
 passport.use(new GoogleStrategy());
